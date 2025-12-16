479b779e5dbfbbd8190c648c19afba86dc2f9a73
Anna Maria <annamariatarallo11@gmail.com>
2025-12-16 15:21:28 +0100

    Initial Commit

---
+++ herzon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  herzon
+//
+//  Created by AFP FED 10 on 16/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ herzon/herzonApp.swift
@@ -0,0 +1,17 @@
+//
+//  herzonApp.swift
+//  herzon
+//
+//  Created by AFP FED 10 on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct herzonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
